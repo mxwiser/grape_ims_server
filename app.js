4d8cd54f3054b531e60cdfae8423814eb4aa6a6b
master <neoway@126.com>
2022-08-18 21:01:26 +0800

    init

--- app.js
+++ app.js
@@ -3,18 +3,22 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/',express.static('./dist'))
 
+
+//routes
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+app.use('/api', usersRouter);
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+
+
 
 module.exports = app;
